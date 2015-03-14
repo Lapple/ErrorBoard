10d8cff8166509f7252f3d1cde622a3ab852c745
Zhihao <lzh727@126.com>
2015-03-14 19:54:39 +0800

    Decode message

--- server/route-reports.js
+++ server/route-reports.js
@@ -7,12 +7,18 @@ module.exports = function(req, res) {
     var type = req.params.type;
     var aggregator = aggregators[type];
 
+    var query = _.transform(req.query, function(result, value, key) {
+        result[key] = decodeURIComponent(value);
+    }, {});
+
+    console.log(query);
+
     if (aggregator) {
         db.find({}, function(err, docs) {
             if (err) {
                 res.json(400, { error: err });
             } else {
-                res.json(_.reduce(docs, aggregator(req.query), {}));
+                res.json(_.reduce(docs, aggregator(query), {}));
             }
         });
     } else {
