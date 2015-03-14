2ca5835660a6051b13d8c2497263820ec5ae5343
Zhihao Li <codinfox@users.noreply.github.com>
2015-03-14 17:14:48 +0800

    Double encode data
    fix #18

--- client/component-app.jsx
+++ client/component-app.jsx
@@ -46,7 +46,7 @@ module.exports = React.createClass({
         }
     },
     _showDetails: function(data) {
-        var url = '/' + this.props.params.type + '/' + encodeURIComponent(data.key) + '/';
+        var url = '/' + this.props.params.type + '/' + encodeURIComponent(encodeURIComponent(data.key)) + '/';
         page.show(url, {details: data.title});
     },
     _hideDetails: function() {
